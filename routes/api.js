5e446050a5ca2625f499c5098569fd7c62b0fdc6
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-18 09:07:51 -0500

    get api request test

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,13 @@ const Workout = require("../models/workout.js")
 
 // view all workouts
 router.get("/api/workouts", (req, res) => {
-    
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 // view range of workouts
