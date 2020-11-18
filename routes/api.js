9b9d221b6ba6bb156ed6d977b9e902186b285a85
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-18 09:03:16 -0500

    added html.js and updated api.js to include shell for api routes (get, post, put, and delete

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
+const Workout = require("../models/workout.js")
 
+// view all workouts
+router.get("/api/workouts", (req, res) => {
+    
+});
+
+// view range of workouts
+router.get("/api/workouts", (req, res) => {
+    
+});
+
+// add workouts 
+router.post("/api/workouts", (req, res) => {
+
+});
+
+// edit workouts
+router.put("/api/workouts", (req, res) => {
+    
+});
+
+
+// delete workouts
+router.delete("/api/workouts", (req, res) => {
+    
+});
 
 module.exports = router;
+
